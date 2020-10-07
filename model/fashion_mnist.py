7ce580aa751ad2455d5de7a2d7641c7e94c1e9e3
ruby73 <ouueyeb1@gmail.com>
2020-10-07 17:46:17 +0900

    17:46

--- model/fashion_mnist.py
+++ model/fashion_mnist.py
@@ -23,21 +23,33 @@ class FashionMnist:
         model = self.create_model()
         model = self.train_model(model, image_list[0], image_list[1])
         # self.test_model(model, image_list[2], image_list[3])
-        self.model_predict(model, image_list[2], image_list[3], 0)
-        
+        arr = self.model_predict(model, image_list[2], image_list[3], 0)
+        # [predictions, test_images, test_labels]
+        predictions = arr[0]
+        test_labels = arr[2]
+        test_images = arr[1]
+        i = 5
+
+        plt.figure(figsize=(6,3))
+        plt.subplot(1, 2, 1)
+        self.plot_image(i, predictions, test_labels, test_images)
+        plt.subplot(1, 2, 2)
+        self.plot_value_array(i, predictions, test_labels)
+        plt.show()
+        print('=============== END ================')
+
 
     def get_data(self) -> []:
         (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
-
+        """
         print(f'훈련 행 {train_images.shape[0]} 열 {train_images.shape[1]}')
         print(f'테스트 행 {test_images.shape[0]} 열 {test_images.shape[1]}')
-
         plt.figure()
         plt.imshow(train_images[3])
         plt.colorbar()
         plt.grid(False)
         plt.show()
-
+        """
         return [train_images, train_labels, test_images, test_labels]
 
     def preprocess(self, train_images, train_labels):
@@ -86,7 +98,7 @@ class FashionMnist:
         predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
         plt.grid(False)
         plt.xticks([])
-        plt.yticks([]) 
+        plt.yticks([])
         plt.imshow(img, cmap=plt.cm.binary)
 
         prediction_label = np.argmax(predictions_array)
@@ -116,3 +128,4 @@ class FashionMnist:
 if __name__ == '__main__':
     fashion = FashionMnist()
     fashion.hook()
+    
